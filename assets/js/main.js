a25db2908aa967f0676b197b5098b83e54aae1bf
Gerardo Vega Martínez <gvega9610martinez@gmail.com>
2022-08-18 01:22:33 -0500

    version final 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,50 @@ function load () {
 
 const menu = document.querySelector("ul.nav-menu");
 const iconMenu = document.getElementById("icon-menu");
+const itemMenu1 = document.getElementById("url1");
+const itemMenu2 = document.getElementById("url2");
+const itemMenu3 = document.getElementById("url3");
+const itemMenu4 = document.getElementById("url4");
+const itemMenu5 = document.getElementById("url5");
+const itemMenu6 = document.getElementById("url6");
 
 iconMenu.addEventListener ("click", () => 
     menu.classList.toggle("active")
 );
+
+itemMenu1.addEventListener ( "click" , () =>
+    menu.classList.remove("active")
+);
+
+itemMenu2.addEventListener ( "click" , () =>
+    menu.classList.remove("active")
+);
+
+itemMenu3.addEventListener ( "click" , () =>
+    menu.classList.remove("active")
+);
+
+itemMenu4.addEventListener ( "click" , () =>
+    menu.classList.remove("active")
+);
+
+itemMenu5.addEventListener ( "click" , () =>
+    menu.classList.remove("active")
+);
+
+itemMenu6.addEventListener ( "click" , () =>
+    menu.classList.remove("active")
+);
+const navBar = document.getElementById("nav-container")
+
+window.addEventListener( "scroll", () =>{
+    if( window.scrollY >= 50 ){
+        navBar.classList.add("scroll-header")
+    }else{
+        navBar.classList.remove("scroll-header")
+    }
+})
+
 
 
 /*===========================carrito de compras=========================*/
